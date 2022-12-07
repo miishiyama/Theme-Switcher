a7a4412dea613abcff32b7af64a7961f5c2f192c
Millene Ishiyama <milleneeishiyama@gmail.com>
2022-12-06 22:02:35 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let ball = document.querySelector(".ball");
+let sun = document.querySelector(".sun");
+let moon = document.querySelector(".moon");
+let body = document.querySelector(".body");
+
+ball.onclick = function() {
+    ball.classList.toggle("light");
+    sun.classList.toggle("light");
+    moon.classList.toggle("light");
+    body.classList.toggle("light");
+}
