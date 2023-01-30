9cfcc8e3edcfc9f4d17bccf224fb5ac50680eba2
Millene Ishiyama <milleneeishiyama@gmail.com>
2023-01-30 18:54:21 -0300

    Organizing code

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let ball = document.querySelector(".ball");
-let sun = document.querySelector(".sun");
-let moon = document.querySelector(".moon");
-let body = document.querySelector(".body");
+let ball = document.querySelector(".ball")
+
+let sun = document.querySelector(".sun")
+
+let moon = document.querySelector(".moon")
+
+let body = document.querySelector(".body")
 
 ball.onclick = function() {
-    ball.classList.toggle("light");
-    sun.classList.toggle("light");
-    moon.classList.toggle("light");
-    body.classList.toggle("light");
+    ball.classList.toggle("light")
+    sun.classList.toggle("light")
+    moon.classList.toggle("light")
+    body.classList.toggle("light")
 }
